2336c2a6a81e402bff73c420355874a06e99869e
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-06-11 10:59:15 -0300

    ok

--- limpa_containers_imagens_docker.sh
+++ limpa_containers_imagens_docker.sh
@@ -3,12 +3,24 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo " "
 echo "Arquivo limpa_containers_imagens_docker.sh iniciado!"
 echo " "
-echo "Verssão 1.02"
+echo "Verssão 1.03"
 echo " "
 
-docker stop $(docker ps -q)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
+echo "Escolha a opção de limpeza:"
+echo "1 - Limpeza completa"
+echo "2 - Limpeza apenas de containers"
+read -p "Digite sua opção (1 ou 2): " user_choice
+
+if [ "$user_choice" = "2" ]; then
+  docker stop $(docker ps -q)
+  docker rm $(docker ps -aq)
+else
+  docker stop $(docker ps -q)
+  docker rm $(docker ps -aq)
+  docker rmi $(docker images -q)
+fi
+
+
 
 echo " "
 echo "Arquivo limpa_containers_imagens_docker.sh terminado com sucesso!"
