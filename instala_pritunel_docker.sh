2ca50c54fc94e17b6692cacd902dced9fde34548
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-06-11 09:54:11 -0300

    ok

--- instala_pritunel_docker.sh
+++ instala_pritunel_docker.sh
@@ -5,13 +5,13 @@ echo "Arquivo instala_pritunel_docker.sh iniciado!"
 echo " "
 echo "Docuemntação: https://github.com/jippi/docker-pritunl"
 echo " "
-echo "Verssão 1.00"
+echo "Verssão 1.01"
 echo " "
 
-data_dir=$(pwd)/data
+DATA_DIR=${pwd}/data
 
-mkdir -p $(data_dir)/pritunl $(data_dir)/mongodb
-touch $(data_dir)/pritunl.conf
+mkdir -p ${DATA_DIR}/pritunl ${DATA_DIR}/mongodb
+touch ${DATA_DIR}/pritunl.conf
 
 docker run \
     --name pritunl \
@@ -20,9 +20,9 @@ docker run \
     --dns 127.0.0.1 \
     --restart=unless-stopped \
     --detach \
-    --volume $(data_dir)/pritunl.conf:/etc/pritunl.conf \
-    --volume $(data_dir)/pritunl:/var/lib/pritunl \
-    --volume $(data_dir)/mongodb:/var/lib/mongodb \
+    --volume ${DATA_DIR}/pritunl.conf:/etc/pritunl.conf \
+    --volume ${DATA_DIR}/pritunl:/var/lib/pritunl \
+    --volume ${DATA_DIR}/mongodb:/var/lib/mongodb \
     ghcr.io/jippi/docker-pritunl
 
 # Esperar um pouco para o container iniciar
