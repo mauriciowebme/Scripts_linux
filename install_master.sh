f34de4bef9fb0ab9281a6d4a968128e0f20de5e4
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-06-12 11:24:36 -0300

    ok

--- install_master.sh
+++ install_master.sh
@@ -13,6 +13,7 @@ echo "1 - Atualização completa sem reinicialização (default)"
 echo "2 - Atualização completa com reinicialização"
 echo "3 - Atualização mínima sem reinicialização"
 echo "4 - Verificar status do sistema"
+echo "5 - Instala docker"
 read -p "Digite sua opção (1, 2, 3 ou 4): " user_choice
 
 case "$user_choice" in
@@ -31,6 +32,31 @@ case "$user_choice" in
     uptime
     echo "Espaço em disco:"
     df -h
+    ;;
+  5)
+    echo "instalando docker..."
+    apt update && apt upgrade -y
+    #https://docs.docker.com/engine/install/ubuntu/
+
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    sudo docker run --name hello hello-world
     ;;
   *)
     echo "Realizando atualização completa sem reiniciar..."
