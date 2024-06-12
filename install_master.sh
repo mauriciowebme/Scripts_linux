a266ae864725ce1e08a280ccc928d94d5c1787ed
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-06-12 13:52:18 -0300

    ok

--- install_master.sh
+++ install_master.sh
@@ -7,20 +7,7 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo " "
 echo "Arquivo install_master.sh iniciado!"
 echo " "
-echo "Versão 1.10"
-echo " "
-
-echo "Escolha a opção:"
-echo "Pressione enter para sair (default)"
-echo "1 - Atualização completa sem reinicialização"
-echo "2 - Atualização completa com reinicialização"
-echo "3 - Atualização mínima sem reinicialização"
-echo "4 - Verificar status do sistema"
-echo "5 - Instala docker"
-echo "6 - Instala mongodb docker"
-echo "7 - Instala pritunel docker"
-echo "8 - Instala postgres docker"
-read -p "Digite sua opção: " user_choice
+echo "Versão 1.11"
 echo " "
 
 instala_docker(){
@@ -49,95 +36,75 @@ instala_docker(){
     sudo docker run --name hello hello-world
 }
 
-case "$user_choice" in
-  1)
-    echo "Atualizando o sistema..."
-    apt update && apt upgrade -y
-    ;;
-  2)
-    echo "Atualizando e reiniciando o sistema..."
-    apt update && apt upgrade -y
-    reboot
-    ;;
-  3)
-    echo "Realizando atualização mínima..."
-    apt update && apt upgrade --with-new-pkgs -y
-    ;;
-  4)
-    echo "Verificando status do sistema..."
-    echo "Uptime do sistema:"
-    uptime
-    echo "Espaço em disco:"
-    df -h
-    ;;
-  5)
-    instala_docker
-    ;;
-  6)
+instala_mongdb(){
     echo "instalando o docker mongo..."
     docker run \
         --name mongodb \
         -d mongo
-    ;;
-  7)
+}
+
+instala_pritunel(){
     echo "instalando pritunel docker..."
 
     if ! command -v docker &> /dev/null; then
         echo "Docker não está instalado."
         instala_docker
     else
-        # Definição do diretório padrão
-        DEFAULT_DIR="/pritunl"
-
-        # Solicita ao usuário para escolher entre o local padrão ou um customizado
-        echo "Escolha a opção de instalação:"
-        echo "1 - Local padrão ($DEFAULT_DIR) (default)"
-        echo "2 - Especificar local manualmente"
-        read -p "Digite sua opção (1 ou 2): " user_choice
-
-        if [ "$user_choice" = "2" ]; then
-            read -p "Informe o diretório de instalação: " DATA_DIR
-        else
-            DATA_DIR=$DEFAULT_DIR
-        fi
-
-        rm -r ${DATA_DIR}
-
-        # Cria a estrutura de diretórios e arquivos necessários
-        echo "Instalação: ${DATA_DIR}"
-        mkdir -p ${DATA_DIR}/pritunl ${DATA_DIR}/mongodb
-        touch ${DATA_DIR}/pritunl.conf
-
-        # Tenta remover o container se existir
-        docker rm -f pritunl
-
-        # Executa o container Docker
-        docker run \
-            --name pritunl \
-            --privileged \
-            --publish 80:80 \
-            --publish 443:443 \
-            --publish 1194:1194 \
-            --publish 1194:1194/udp \
-            --dns 127.0.0.1 \
-            --restart=unless-stopped \
-            --detach \
-            --volume $(data_dir)/pritunl.conf:/etc/pritunl.conf \
-            --volume $(data_dir)/pritunl:/var/lib/pritunl \
-            --env PRITUNL_MONGODB_URI=mongodb://127.0.0.1:27017/pritunl \
-            ghcr.io/jippi/docker-pritunl
-
-        # Espera um pouco para o container iniciar
-        sleep 20
-        echo " "
-
-        # Redefine a senha do Pritunl
-        docker exec pritunl pritunl reset-password
-        echo "Possiveis ip para acesso"
-        hostname -I | tr ' ' '\n'
+        echo "Docker instalado ok."
     fi
-    ;;
-  8)
+    container_name="mongodb"
+    if [ $(docker ps -q -f name=^/${container_name}$) ]; then
+        echo "O container $container_name já está em execução."
+    else
+        echo "O container $container_name não existe ou não está em execução. Criando e iniciando o container..."
+        # Comando para criar e iniciar o container MongoDB
+        docker run --name $container_name -d mongo
+        echo "Container $container_name criado e iniciado com sucesso."
+    fi
+    # Definição do diretório padrão
+    DEFAULT_DIR="/pritunl"
+    # Solicita ao usuário para escolher entre o local padrão ou um customizado
+    echo "Escolha a opção de instalação:"
+    echo "1 - Local padrão ($DEFAULT_DIR) (default)"
+    echo "2 - Especificar local manualmente"
+    read -p "Digite sua opção (1 ou 2): " user_choice
+    if [ "$user_choice" = "2" ]; then
+        read -p "Informe o diretório de instalação: " DATA_DIR
+    else
+        DATA_DIR=$DEFAULT_DIR
+    fi
+    rm -r ${DATA_DIR}
+    # Cria a estrutura de diretórios e arquivos necessários
+    echo "Instalação: ${DATA_DIR}"
+    mkdir -p ${DATA_DIR}/pritunl ${DATA_DIR}/mongodb
+    touch ${DATA_DIR}/pritunl.conf
+    # Tenta remover o container se existir
+    docker rm -f pritunl
+    # Executa o container Docker
+    docker run \
+        --name pritunl \
+        --privileged \
+        --publish 80:80 \
+        --publish 443:443 \
+        --publish 1194:1194 \
+        --publish 1194:1194/udp \
+        --dns 127.0.0.1 \
+        --restart=unless-stopped \
+        --detach \
+        --volume $(data_dir)/pritunl.conf:/etc/pritunl.conf \
+        --volume $(data_dir)/pritunl:/var/lib/pritunl \
+        --env PRITUNL_MONGODB_URI=mongodb://127.0.0.1:27017/pritunl \
+        ghcr.io/jippi/docker-pritunl
+    # Espera um pouco para o container iniciar
+    sleep 20
+    echo " "
+    # Redefine a senha do Pritunl
+    docker exec pritunl pritunl reset-password
+    echo "Possiveis ip para acesso"
+    hostname -I | tr ' ' '\n'
+}
+
+instala_postgres(){
     echo "Instalando postgres docker..."
         
     # Remover container existente se houver
@@ -164,10 +131,57 @@ case "$user_choice" in
 
     # Reiniciar o PostgreSQL para aplicar configurações
     docker restart postgres
-        ;;
-    *)
-        echo "Nada foi execudato!"
 
+}
+
+echo "Escolha a opção:"
+echo "Pressione enter para sair (default)"
+echo "1 - Atualização completa sem reinicialização"
+echo "2 - Atualização completa com reinicialização"
+echo "3 - Atualização mínima sem reinicialização"
+echo "4 - Verificar status do sistema"
+echo "5 - Instala docker"
+echo "6 - Instala mongodb docker"
+echo "7 - Instala pritunel docker"
+echo "8 - Instala postgres docker"
+read -p "Digite sua opção: " user_choice
+echo " "
+
+case "$user_choice" in
+  1)
+    echo "Atualizando o sistema..."
+    apt update && apt upgrade -y
+    ;;
+  2)
+    echo "Atualizando e reiniciando o sistema..."
+    apt update && apt upgrade -y
+    reboot
+    ;;
+  3)
+    echo "Realizando atualização mínima..."
+    apt update && apt upgrade --with-new-pkgs -y
+    ;;
+  4)
+    echo "Verificando status do sistema..."
+    echo "Uptime do sistema:"
+    uptime
+    echo "Espaço em disco:"
+    df -h
+    ;;
+  5)
+    instala_docker
+    ;;
+  6)
+    instala_mongdb
+    ;;
+  7)
+    instala_pritunel
+    ;;
+  8)
+    instala_postgres
+    ;;
+  8)
+    echo "Nada executado!"
     ;;
 esac
 
