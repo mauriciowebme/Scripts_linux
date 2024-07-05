90a2af717b177ac45dcb258e39983484a7efb3cd
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-07-05 09:06:00 -0300

    ok

--- install_master.sh
+++ install_master.sh
@@ -162,7 +162,7 @@ instala_pritunel_docker(){
         ghcr.io/jippi/docker-pritunl
 
     echo "Aguarde enquanto o container é inicializado..."
-    sleep 20
+    sleep 30
 
     # Configuração inicial pós-instalação
     sudo docker exec pritunl pritunl reset-password
