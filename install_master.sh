c4d2d8b85dbdfc261888fdcd1f31c639b787b910
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-07-26 12:32:50 +0000

    ok

--- install_master.sh
+++ install_master.sh
@@ -1193,7 +1193,8 @@ padrao_ubuntu(){
 }
 
 vscode_server(){
-    docker run -d --name vscode_server -p 8081:8080 -v "/projects:/home/coder/projects" -e PASSWORD="1234" codercom/code-server:latest
+    read -p "Digite uma senha: " SENHA
+    docker run -d --name vscode_server -p 8081:8080 -v "/projects:/home/coder/projects" -e PASSWORD="$SENHA" codercom/code-server:latest
     echo "IPs possíveis para acesso:"
     hostname -I | tr ' ' '\n'
     echo "Porta: 8081"
