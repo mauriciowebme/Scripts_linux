efc2bc493030da2cab2fd75b431c0ef59b6f45bc
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-11-04 11:56:11 -0300

    ok

--- install_master.py
+++ install_master.py
@@ -47,6 +47,7 @@ class Executa_comados():
             processo.wait()
             if processo.returncode != 0:
                 print(f"\nErro ao executar comando: {comando}\n")
+                resultados[comando] += ['Erro:True']
                 for linha in processo.stderr:
                     print(linha, end="")
                 if not ignorar_erros:
@@ -183,26 +184,34 @@ class Docker(Executa_comados):
             # print("Docker está instalado.")
         else:
             print("Docker não está instalado.")
-            comandos = [
-                "apt update && apt upgrade -y",
-                "for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done",
-                "sudo apt-get update",
-                "sudo apt-get install -y ca-certificates curl",
-                "sudo install -m 0755 -d /etc/apt/keyrings",
-                "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
-                "sudo chmod a+r /etc/apt/keyrings/docker.asc",
-                (
-                    "echo "
-                    "'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "
-                    "https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable' | "
-                    "sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
-                ),
-                "sudo apt-get update",
-                "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-            ]
+            for i in range(2):
+                comandos = [
+                    "apt update && apt upgrade -y",
+                    "for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done",
+                    "sudo apt-get update",
+                    "sudo apt-get install -y ca-certificates curl",
+                    "sudo install -m 0755 -d /etc/apt/keyrings",
+                    "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
+                    "sudo chmod a+r /etc/apt/keyrings/docker.asc",
+                    (
+                        "echo "
+                        "'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "
+                        "https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable' | "
+                        "sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+                    ),
+                    "sudo apt-get update",
+                    "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+                ]
 
-            # Executa todos os comandos de instalação do Docker
-            self.executar_comandos(comandos)
+                # Executa todos os comandos de instalação do Docker
+                resultados = self.executar_comandos(comandos, ignorar_erros=True)
+                if 'erro:true' not in resultados[-1]:
+                    break
+                else:
+                    comandos = [
+                        f"""sudo rm /etc/apt/sources.list.d/docker.list""",
+                        ]
+                    self.executar_comandos(comandos)
             self.cria_rede_docker()
 
 class Sistema(Docker, Executa_comados):
