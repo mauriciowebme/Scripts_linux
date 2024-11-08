349693defeba72fd990a68c22ff5699b282ebe42
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-11-08 16:25:21 -0300

    ok

--- install_master.py
+++ install_master.py
@@ -417,7 +417,7 @@ certificatesResolvers:
         dominio = input('Digite o dominio:')
         
         dominio_ = dominio.replace('.', '_')
-        comando = f"docker exec -i mysql_wordpress mysql -uwordpress -pwordpress -e \"CREATE DATABASE {dominio_}; GRANT ALL PRIVILEGES ON {dominio_}.* TO 'wordpress'@'%'; FLUSH PRIVILEGES;\""
+        comando = f"docker exec -i mysql_wordpress mysql -uroot -pwordpress -e \"CREATE DATABASE {dominio_}; GRANT ALL PRIVILEGES ON {dominio_}.* TO 'wordpress'@'%'; FLUSH PRIVILEGES;\""
         self.executar_comandos([comando])
         container = f"""docker run -d \
                         --name wp_{dominio_} \
