d3e65bf4a22476018f1c707ca269e3fa21b86174
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-11-24 18:36:40 -0300

    ok

--- install_master.py
+++ install_master.py
@@ -737,7 +737,7 @@ app.listen(PORT, () => {{
         if not os.path.exists(self.atmoz_sftp_arquivo_conf):
             os.makedirs(os.path.dirname(self.atmoz_sftp_arquivo_conf), exist_ok=True)
             with open(self.atmoz_sftp_arquivo_conf, "w") as arquivo:
-                arquivo.write(f"teste:teste:::/dados")
+                arquivo.write(f"teste:teste::/dados")
             print(f"Arquivo users.conf criado.")
             
         """
@@ -758,7 +758,7 @@ app.listen(PORT, () => {{
                         --name atmoz_sftp \
                         --restart=always \
                         -p 2222:22 \
-                        -v {self.install_principal}/dir_ftp:/home \
+                        -v {self.install_principal}/atmoz_sftp:/home \
                         -v {self.atmoz_sftp_arquivo_conf}:/etc/sftp/users.conf:ro \
                         atmoz/sftp
                     """
