931d232fb591a37f4e1e824e2d8c8c81a202c79c
Mauricio Azeredo <mauriciowebme@gmail.com>
2024-12-30 19:46:03 -0300

    remove configuração de "watch" para o package.json no install_master.py

--- install_master.py
+++ install_master.py
@@ -1084,11 +1084,10 @@ WantedBy=timers.target
             with open(caminho_sftp_json, "w") as arquivo:
                 json.dump(sftp_json, arquivo, indent=4)
             print(f"Arquivo nodemon.json criado em {caminho_sftp_json}")
-            
+        # "watch": [
+        #         "package.json"
+        #     ],
         nodemon_json = {
-            "watch": [
-                "package.json"
-            ],
             "ignore": [
                 "package-lock.json",
                 "arquivos",
