ba1d013f7188f9bc5cb1e3169a6dc7edcb633cf5
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-04-09 18:28:06 -0300

    altera o tempo limite da sessão do nó Selenium e ajusta o parâmetro de sessão

--- install_master.py
+++ install_master.py
@@ -2445,8 +2445,8 @@ CMD ["sh", "-c", "\
         container = f"""docker run -d \
                 --name selenium-firefox \
                 --restart=unless-stopped \
-                -e SE_SESSION_TIMEOUT=30 \
                 -e SE_NODE_MAX_SESSIONS=2 \
+                -e SE_NODE_SESSION_TIMEOUT=20 \
                 -p 4444:4444 \
                 -p 7900:7900 \
                 --shm-size=2g \
