f23b001f9e472887fb9ab9fb38bf9c76a23480e3
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-04-25 14:40:33 -0300

    corrige definição da função setup_inicializar_service para incluir 'self'

--- install_master.py
+++ install_master.py
@@ -3193,7 +3193,7 @@ class Sistema(Docker, Executa_comados):
         # Executar o comando sensors
         self.executar_comandos(["speedtest "], comando_direto=True)
 
-    def setup_inicializar_service():
+    def setup_inicializar_service(self):
         """
         1. Cria /install_principal/inicializar.py (com log simples).
         2. Cria /etc/systemd/system/inicializar.service apontando para ele.
