7a81803d65910ffe92f05816dd702c15da86cb16
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-04-29 15:39:35 -0300

    adiciona suporte a pt_BR e configuração do teclado ABNT2 no Dockerfile

--- install_master.py
+++ install_master.py
@@ -2538,77 +2538,77 @@ CMD ["sh", "-c", "\
             unzip p7zip-full unrar \
             file-roller xarchiver thunar-archive-plugin
             
-        RUN echo 'Instala dpendencias do chrome'
-        RUN apt-get install -y \
-            libxss1 \
-            libappindicator3-1 \
-            libindicator7 \
-            fonts-liberation \
-            libatk-bridge2.0-0 \
-            libgtk-3-0 \
-            xdg-utils \
-            libgbm-dev \
-            dbus-x11
+        # RUN echo 'Instala dpendencias do chrome'
+        # RUN apt-get install -y \
+        #     libxss1 \
+        #     libappindicator3-1 \
+        #     libindicator7 \
+        #     fonts-liberation \
+        #     libatk-bridge2.0-0 \
+        #     libgtk-3-0 \
+        #     xdg-utils \
+        #     libgbm-dev \
+        #     dbus-x11
         
-        RUN echo 'Instala o Google Chrome'
-        RUN wget -q -O - https://dl.google.com/linux/linux_signing_key.pub \
-            | gpg --dearmor > /etc/apt/keyrings/google-chrome.gpg && \
-            echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] \
-            http://dl.google.com/linux/chrome/deb/ stable main" \
-            > /etc/apt/sources.list.d/google-chrome.list && \
-            apt-get update && \
-            apt-get install -y --no-install-recommends google-chrome-stable && \
-            apt-get clean && rm -rf /var/lib/apt/lists/*
+        # RUN echo 'Instala o Google Chrome'
+        # RUN wget -q -O - https://dl.google.com/linux/linux_signing_key.pub \
+        #     | gpg --dearmor > /etc/apt/keyrings/google-chrome.gpg && \
+        #     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] \
+        #     http://dl.google.com/linux/chrome/deb/ stable main" \
+        #     > /etc/apt/sources.list.d/google-chrome.list && \
+        #     apt-get update && \
+        #     apt-get install -y --no-install-recommends google-chrome-stable && \
+        #     apt-get clean && rm -rf /var/lib/apt/lists/*
             
-        RUN echo 'Instala atalho Google Chrome'
-        RUN echo 'Cria o wrapper sem here-doc'
-        RUN mkdir -p /usr/local/bin && \
-            echo '#!/bin/bash' > /usr/local/bin/chrome-wrapper.sh && \
-            echo 'exec /usr/bin/google-chrome-stable --no-sandbox --disable-setuid-sandbox --disable-dev-shm-usage --disable-gpu "$@"' \
-            >> /usr/local/bin/chrome-wrapper.sh && \
-            chmod +x /usr/local/bin/chrome-wrapper.sh
+        # RUN echo 'Instala atalho Google Chrome'
+        # RUN echo 'Cria o wrapper sem here-doc'
+        # RUN mkdir -p /usr/local/bin && \
+        #     echo '#!/bin/bash' > /usr/local/bin/chrome-wrapper.sh && \
+        #     echo 'exec /usr/bin/google-chrome-stable --no-sandbox --disable-setuid-sandbox --disable-dev-shm-usage --disable-gpu "$@"' \
+        #     >> /usr/local/bin/chrome-wrapper.sh && \
+        #     chmod +x /usr/local/bin/chrome-wrapper.sh
 
-        RUN echo 'Cria o atalho do Chrome no menu apontando pro wrapper'
-        RUN mkdir -p /usr/share/applications && \
-            echo '[Desktop Entry]'                                       > /usr/share/applications/google-chrome.desktop && \
-            echo 'Name=Google Chrome'                                   >> /usr/share/applications/google-chrome.desktop && \
-            echo 'Comment=Navegador Web'                                >> /usr/share/applications/google-chrome.desktop && \
-            echo 'Exec=/usr/local/bin/chrome-wrapper.sh %U'             >> /usr/share/applications/google-chrome.desktop && \
-            echo 'Terminal=false'                                       >> /usr/share/applications/google-chrome.desktop && \
-            echo 'Type=Application'                                     >> /usr/share/applications/google-chrome.desktop && \
-            echo 'Icon=google-chrome'                                   >> /usr/share/applications/google-chrome.desktop && \
-            echo 'Categories=Network;WebBrowser'                        >> /usr/share/applications/google-chrome.desktop && \
-            echo 'StartupNotify=true'                                   >> /usr/share/applications/google-chrome.desktop && \
-            chmod +x /usr/share/applications/google-chrome.desktop
+        # RUN echo 'Cria o atalho do Chrome no menu apontando pro wrapper'
+        # RUN mkdir -p /usr/share/applications && \
+        #     echo '[Desktop Entry]'                                       > /usr/share/applications/google-chrome.desktop && \
+        #     echo 'Name=Google Chrome'                                   >> /usr/share/applications/google-chrome.desktop && \
+        #     echo 'Comment=Navegador Web'                                >> /usr/share/applications/google-chrome.desktop && \
+        #     echo 'Exec=/usr/local/bin/chrome-wrapper.sh %U'             >> /usr/share/applications/google-chrome.desktop && \
+        #     echo 'Terminal=false'                                       >> /usr/share/applications/google-chrome.desktop && \
+        #     echo 'Type=Application'                                     >> /usr/share/applications/google-chrome.desktop && \
+        #     echo 'Icon=google-chrome'                                   >> /usr/share/applications/google-chrome.desktop && \
+        #     echo 'Categories=Network;WebBrowser'                        >> /usr/share/applications/google-chrome.desktop && \
+        #     echo 'StartupNotify=true'                                   >> /usr/share/applications/google-chrome.desktop && \
+        #     chmod +x /usr/share/applications/google-chrome.desktop
         
-        # RUN echo 'Habilita universe (se ainda não fez)'
-        # RUN apt-get update \
-        #     && apt-get install -y --no-install-recommends software-properties-common \
-        #     && add-apt-repository universe \
-        #     && rm -rf /var/lib/apt/lists/*
+        RUN echo 'Habilita universe (se ainda não fez)'
+        RUN apt-get update \
+            && apt-get install -y --no-install-recommends software-properties-common \
+            && add-apt-repository universe \
+            && rm -rf /var/lib/apt/lists/*
         
-        # RUN echo 'Instala suporte a pt_BR e IBus para XFCE'
-        # RUN apt-get update \
-        #     && apt-get install -y --no-install-recommends \
-        #     locales \
-        #     language-pack-pt-base \
-        #     hunspell-pt-br \
-        #     ibus \
-        #     ibus-gtk \
-        #     ibus-gtk3 
+        RUN echo 'Instala suporte a pt_BR e IBus para XFCE'
+        RUN apt-get update \
+            && apt-get install -y --no-install-recommends \
+            locales \
+            language-pack-pt-base \
+            hunspell-pt-br \
+            ibus \
+            ibus-gtk \
+            ibus-gtk3 
 
-        # RUN echo 'Gera e define o locale pt_BR.UTF-8'
-        # RUN sed -i 's/# pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/' /etc/locale.gen \
-        #     && locale-gen pt_BR.UTF-8 \
-        #     && update-locale LANG=pt_BR.UTF-8 LANGUAGE=pt_BR:pt LC_ALL=pt_BR.UTF-8
+        RUN echo 'Gera e define o locale pt_BR.UTF-8'
+        RUN sed -i 's/# pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/' /etc/locale.gen \
+            && locale-gen pt_BR.UTF-8 \
+            && update-locale LANG=pt_BR.UTF-8 LANGUAGE=pt_BR:pt LC_ALL=pt_BR.UTF-8
 
-        # RUN echo 'Define como default para todo o container'
-        # ENV LANG=pt_BR.UTF-8 \
-        #     LANGUAGE=pt_BR:pt \
-        #     LC_ALL=pt_BR.UTF-8
+        RUN echo 'Define como default para todo o container'
+        ENV LANG=pt_BR.UTF-8 \
+            LANGUAGE=pt_BR:pt \
+            LC_ALL=pt_BR.UTF-8
             
-        # RUN echo 'Adiciona ABNT2 ao iniciar o XFCE'
-        # RUN echo "setxkbmap -layout br -variant abnt2" >> /etc/xdg/xfce4/xinitrc
+        RUN echo 'Adiciona ABNT2 ao iniciar o XFCE'
+        RUN echo "setxkbmap -layout br -variant abnt2" >> /etc/xdg/xfce4/xinitrc
         
         RUN echo 'Realiza limpeza'
         RUN apt-get clean && rm -rf /var/lib/apt/lists/*
