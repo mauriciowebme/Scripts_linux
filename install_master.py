062440ee94812333a2f1745d498a67c339300c29
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-05-02 13:57:53 -0300

    refatora install_master.py: corrige a formatação da senha root no comando MySQL para evitar problemas de interpretação

--- install_master.py
+++ install_master.py
@@ -2020,7 +2020,7 @@ module.exports = { setupPythonEnv, runPythonScript };
             # Remove o usuário root com host '%', mantendo apenas o usuário root com host 'localhost'.
             time.sleep(30)
             comandos = [
-                f"docker exec -i mysql_{versao_}_slave mysql -uroot -p{self.mysql_root_password} -e \"UPDATE mysql.user SET Host='172.%' WHERE User='root' AND Host='%'; FLUSH PRIVILEGES;\""
+                f"docker exec -i mysql_{versao_}_slave mysql -uroot -p'{self.mysql_root_password}' -e \"UPDATE mysql.user SET Host='172.%' WHERE User='root' AND Host='%'; FLUSH PRIVILEGES;\""
             ]
             self.executar_comandos(comandos)
             
