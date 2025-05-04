75932e34b71defda9d3c9c9419f623a42e15e12e
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-05-04 13:34:34 -0300

    refatora install_master.py: ajusta a formatação do conteúdo de configuração do Prometheus para corrigir a indentação

--- install_master.py
+++ install_master.py
@@ -427,17 +427,18 @@ class Docker(Executa_comados):
         print("Iniciando instalação de monitoramento com Prometheus, Node Exporter e Grafana.")
         conteudo = textwrap.dedent("""\
         global:
-            scrape_interval: 15s
+          scrape_interval: 15s
 
         scrape_configs:
-            - job_name: 'prometheus'
-                static_configs:
-                - targets: ['localhost:9090']
+          - job_name: 'prometheus'
+            static_configs:
+              - targets: ['localhost:9090']
 
-            - job_name: 'node_exporter'
-                static_configs:
-                - targets: ['node-exporter:9100']
+          - job_name: 'node_exporter'
+            static_configs:
+              - targets: ['node-exporter:9100']
         """)
+
         caminho = f'{self.install_principal}/prometheus/prometheus.yml'
         comandos = [
             f"mkdir -p {self.install_principal}/prometheus/",
