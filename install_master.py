789b113490f13b41293d8b5ba4a0a168ea26888f
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-05-04 13:05:38 -0300

    refatora install_master.py: ajusta a formatação do conteúdo de monitoramento para corrigir a indentação

--- install_master.py
+++ install_master.py
@@ -426,16 +426,16 @@ class Docker(Executa_comados):
     def iniciar_monitoramento(self):
         conteudo = textwrap.dedent("""\
         global:
-        scrape_interval: 15s
+            scrape_interval: 15s
 
         scrape_configs:
-        - job_name: 'prometheus'
-            static_configs:
-            - targets: ['localhost:9090']
+            - job_name: 'prometheus'
+                static_configs:
+                - targets: ['localhost:9090']
 
-        - job_name: 'node_exporter'
-            static_configs:
-            - targets: ['node-exporter:9100']
+            - job_name: 'node_exporter'
+                static_configs:
+                - targets: ['node-exporter:9100']
         """)
         caminho = f'{self.install_principal}/prometheus/prometheus.yml'
         comandos = [
@@ -466,7 +466,6 @@ class Docker(Executa_comados):
         self.cria_rede_docker(associar_container_nome='prometheus', numero_rede=1)
         self.cria_rede_docker(associar_container_nome='node-exporter', numero_rede=1)
         self.cria_rede_docker(associar_container_nome='grafana', numero_rede=1)
-    
     
     def cria_dynamic_conf_traefik(self,):
         dynamic_conf = f'{self.install_principal}/traefik/dynamic_conf.yml'
