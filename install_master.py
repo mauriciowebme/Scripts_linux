4990233c94072d6dd2d469741e7358f3a037d153
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-05-13 19:24:01 -0300

    adiciona exemplo de configuração JSON para o Grafana na saída do script

--- install_master.py
+++ install_master.py
@@ -489,6 +489,289 @@ class Docker(Executa_comados):
         
         print('Acesse o Grafana em http://localhost:3000')
         print('Usuario e Senha padrão: admin/admin')
+        resposta = input('Deseja ver um exemplo de json para o Grafana?: S ou N: ')
+        if resposta.lower() == 's':
+            print('Acesse o Grafana em http://localhost:3000')
+            print('Usuario e Senha padrão: admin/admin')
+            print('Cole o seguinte json:')
+            print(textwrap.dedent("""\
+            {
+                "annotations": {
+                    "list": [
+                    {
+                        "builtIn": 1,
+                        "datasource": {
+                        "type": "grafana",
+                        "uid": "-- Grafana --"
+                        },
+                        "enable": true,
+                        "hide": true,
+                        "iconColor": "rgba(0, 211, 255, 1)",
+                        "name": "Annotations & Alerts",
+                        "type": "dashboard"
+                    }
+                    ]
+                },
+                "editable": true,
+                "fiscalYearStartMonth": 0,
+                "graphTooltip": 0,
+                "id": 1,
+                "links": [],
+                "panels": [
+                    {
+                    "datasource": {
+                        "type": "prometheus",
+                        "uid": "belsuah7zhqm8d"
+                    },
+                    "fieldConfig": {
+                        "defaults": {
+                        "color": {
+                            "mode": "palette-classic"
+                        },
+                        "custom": {
+                            "axisBorderShow": false,
+                            "axisCenteredZero": false,
+                            "axisColorMode": "text",
+                            "axisLabel": "",
+                            "axisPlacement": "auto",
+                            "barAlignment": 0,
+                            "barWidthFactor": 0.6,
+                            "drawStyle": "line",
+                            "fillOpacity": 0,
+                            "gradientMode": "none",
+                            "hideFrom": {
+                            "legend": false,
+                            "tooltip": false,
+                            "viz": false
+                            },
+                            "insertNulls": false,
+                            "lineInterpolation": "smooth",
+                            "lineStyle": {
+                            "fill": "solid"
+                            },
+                            "lineWidth": 1,
+                            "pointSize": 5,
+                            "scaleDistribution": {
+                            "type": "linear"
+                            },
+                            "showPoints": "auto",
+                            "spanNulls": false,
+                            "stacking": {
+                            "group": "A",
+                            "mode": "none"
+                            },
+                            "thresholdsStyle": {
+                            "mode": "off"
+                            }
+                        },
+                        "mappings": [],
+                        "thresholds": {
+                            "mode": "absolute",
+                            "steps": [
+                            {
+                                "color": "green"
+                            },
+                            {
+                                "color": "red",
+                                "value": 80
+                            }
+                            ]
+                        }
+                        },
+                        "overrides": []
+                    },
+                    "gridPos": {
+                        "h": 8,
+                        "w": 12,
+                        "x": 0,
+                        "y": 0
+                    },
+                    "id": 1,
+                    "options": {
+                        "legend": {
+                        "calcs": [],
+                        "displayMode": "list",
+                        "placement": "bottom",
+                        "showLegend": true
+                        },
+                        "tooltip": {
+                        "hideZeros": false,
+                        "mode": "single",
+                        "sort": "none"
+                        }
+                    },
+                    "pluginVersion": "12.0.0",
+                    "targets": [
+                        {
+                        "datasource": {
+                            "type": "prometheus",
+                            "uid": "felsgfpj8hg5cb"
+                        },
+                        "disableTextWrap": false,
+                        "editorMode": "code",
+                        "expr": "100 - (avg by(instance)(\r\n        rate(node_cpu_seconds_total{mode=\"idle\"}[30s])\r\n      ) * 100)",
+                        "fullMetaSearch": false,
+                        "includeNullMetadata": true,
+                        "legendFormat": "CPU",
+                        "range": true,
+                        "refId": " CPU",
+                        "useBackend": false
+                        },
+                        {
+                        "datasource": {
+                            "type": "prometheus",
+                            "uid": "felsgfpj8hg5cb"
+                        },
+                        "editorMode": "code",
+                        "expr": "100 * (\r\n  1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)\r\n)",
+                        "hide": false,
+                        "instant": false,
+                        "legendFormat": "RAM",
+                        "range": true,
+                        "refId": "RAM usada"
+                        },
+                        {
+                        "datasource": {
+                            "type": "prometheus",
+                            "uid": "felsgfpj8hg5cb"
+                        },
+                        "editorMode": "code",
+                        "expr": "100 * (\r\n  1 - (node_memory_SwapFree_bytes / ignoring(job,instance) node_memory_SwapTotal_bytes)\r\n)\r\n\r\n",
+                        "hide": false,
+                        "instant": false,
+                        "legendFormat": "SWAP",
+                        "range": true,
+                        "refId": "SWAP"
+                        }
+                    ],
+                    "title": "Painel principal",
+                    "type": "timeseries"
+                    },
+                    {
+                    "datasource": {
+                        "type": "prometheus",
+                        "uid": "belsuah7zhqm8d"
+                    },
+                    "fieldConfig": {
+                        "defaults": {
+                        "color": {
+                            "mode": "palette-classic"
+                        },
+                        "custom": {
+                            "axisBorderShow": false,
+                            "axisCenteredZero": false,
+                            "axisColorMode": "text",
+                            "axisLabel": "",
+                            "axisPlacement": "auto",
+                            "barAlignment": 0,
+                            "barWidthFactor": 0.6,
+                            "drawStyle": "line",
+                            "fillOpacity": 0,
+                            "gradientMode": "none",
+                            "hideFrom": {
+                            "legend": false,
+                            "tooltip": false,
+                            "viz": false
+                            },
+                            "insertNulls": false,
+                            "lineInterpolation": "smooth",
+                            "lineWidth": 1,
+                            "pointSize": 5,
+                            "scaleDistribution": {
+                            "type": "linear"
+                            },
+                            "showPoints": "auto",
+                            "spanNulls": false,
+                            "stacking": {
+                            "group": "A",
+                            "mode": "none"
+                            },
+                            "thresholdsStyle": {
+                            "mode": "off"
+                            }
+                        },
+                        "mappings": [],
+                        "thresholds": {
+                            "mode": "absolute",
+                            "steps": [
+                            {
+                                "color": "green"
+                            },
+                            {
+                                "color": "red",
+                                "value": 80
+                            }
+                            ]
+                        },
+                        "unit": "decmbytes"
+                        },
+                        "overrides": []
+                    },
+                    "gridPos": {
+                        "h": 8,
+                        "w": 12,
+                        "x": 12,
+                        "y": 0
+                    },
+                    "id": 2,
+                    "options": {
+                        "legend": {
+                        "calcs": [],
+                        "displayMode": "list",
+                        "placement": "bottom",
+                        "showLegend": true
+                        },
+                        "tooltip": {
+                        "hideZeros": false,
+                        "mode": "single",
+                        "sort": "none"
+                        }
+                    },
+                    "pluginVersion": "12.0.0",
+                    "targets": [
+                        {
+                        "editorMode": "code",
+                        "expr": "rate(node_disk_read_bytes_total{device!~\"^(loop|ram|fd|sr0).*\"}[10s]) / 1024 / 1024",
+                        "legendFormat": "Leitura",
+                        "range": true,
+                        "refId": "A"
+                        },
+                        {
+                        "datasource": {
+                            "type": "prometheus",
+                            "uid": "felsgfpj8hg5cb"
+                        },
+                        "editorMode": "code",
+                        "expr": "rate(node_disk_written_bytes_total{device!~\"^(loop|ram|fd|sr0).*\"}[10s]) / 1024 / 1024\r\n",
+                        "hide": false,
+                        "instant": false,
+                        "legendFormat": "Escrita",
+                        "range": true,
+                        "refId": "B"
+                        }
+                    ],
+                    "title": "DISCO",
+                    "type": "timeseries"
+                    }
+                ],
+                "preload": false,
+                "refresh": "auto",
+                "schemaVersion": 41,
+                "tags": [],
+                "templating": {
+                    "list": []
+                },
+                "time": {
+                    "from": "now-5m",
+                    "to": "now"
+                },
+                "timepicker": {},
+                "timezone": "browser",
+                "title": "Principal dashboard",
+                "uid": "e5dc7514-6689-4a2c-a78f-3be4a3be041e",
+                "version": 4
+            }
+            """))
     
     def cria_dynamic_conf_traefik(self, email=None):
         dynamic_conf = f'{self.install_principal}/traefik/dynamic_conf.yml'
