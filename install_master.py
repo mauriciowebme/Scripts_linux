56fd813ed6319050e5f7063c21d206454866af95
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-05-15 09:18:04 -0300

    refatora Sistema: adiciona quebra de linha na exibição da chave de acesso nos logs do rustdesk

--- install_master.py
+++ install_master.py
@@ -1580,7 +1580,7 @@ WantedBy=timers.target
         resultados = self.executar_comandos(comandos, exibir_resultados=False)[f"docker logs rustdesk-hbbs"]
         for x in resultados:
             if 'Key' in x:
-                print(f"Chave de acesso: {x.split('Key: ')[1]}")
+                print(f"\nChave de acesso: {x.split('Key: ')[1]}")
             
     def instala_portainer(self,):
         self.remove_container('portainer')
