458599abe6904e8b3b9d343895820627a60076ed
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-05-16 09:35:52 -0300

    refatora Sistema: comenta configuração de Docker para backup do MySQL

--- install_master.py
+++ install_master.py
@@ -2447,6 +2447,39 @@ module.exports = { setupPythonEnv, runPythonScript };
         print('Instalando o mysql.\n')
         # self.gerenciar_permissoes_pasta(f"{self.install_principal}/mysql/{versao_}", permissao="777")
         
+        # docker_file = textwrap.dedent(f"""\
+        #     FROM mysql:8.0
+
+        #     # Instala cron
+        #     RUN apt-get update && apt-get install -y cron && rm -rf /var/lib/apt/lists/*
+
+        #     # Cria diretório de backup
+        #     RUN mkdir -p /backup
+
+        #     # Adiciona script de backup diretamente
+        #     RUN echo '#!/bin/bash\n\
+        #     DATE=$(date +%F_%H-%M)\n\
+        #     BACKUP_DIR="/backup"\n\
+        #     MYSQL_USER="root"\n\
+        #     MYSQL_PASS="$MYSQL_ROOT_PASSWORD"\n\
+        #     MYSQL_HOST="localhost"\n\
+        #     mysqldump -h $MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS --all-databases > $BACKUP_DIR/backup_$DATE.sql\n'\
+        #     > /usr/local/bin/backup.sh && chmod +x /usr/local/bin/backup.sh
+
+        #     # Cria agendamento no crontab (todo dia às 2h da manhã)
+        #     RUN echo '0 2 * * * root /usr/local/bin/backup.sh >> /var/log/cron.log 2>&1' > /etc/cron.d/mysql-backup
+
+        #     # Aplica o cron agendado
+        #     RUN crontab /etc/cron.d/mysql-backup
+
+        #     # Cria log para o cron
+        #     RUN touch /var/log/cron.log
+
+        #     # Comando para iniciar cron e MySQL juntos
+        #     CMD service cron start && docker-entrypoint.sh mysqld
+        #     """)
+        # self.executar_comandos_run_OrAnd_dockerfile(dockerfile_str=docker_file, run_cmd=None)
+        
         container_db = f"""docker run -d \
                         --name mysql_{versao_} \
                         --restart=unless-stopped \
