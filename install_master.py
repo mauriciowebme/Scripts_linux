5d05efe2ed6d80f1cf35a553dd455ce9a0d4c8c2
Mauricio Azeredo <mauriciowebme@gmail.com>
2025-05-17 09:39:41 -0300

    refatora Sistema: adiciona máscara para serviços de espera de rede e remove desabilitação duplicada

--- install_master.py
+++ install_master.py
@@ -50,12 +50,13 @@ def check_for_update():
     print("Primeira execução detectada. Atualizando o sistema...")
     subprocess.run("sudo apt-get update".split(), check=False)
     subprocess.run("sudo apt-get upgrade -y".split(), check=False)
+    
     # Evitar espera de rede na inicialização
     subprocess.run("sudo systemctl disable NetworkManager-wait-online.service".split(), check=False)
     subprocess.run("sudo systemctl disable systemd-networkd-wait-online.service".split(), check=False)
+    subprocess.run("sudo systemctl mask systemd-networkd-wait-online.service".split(), check=False)
+    subprocess.run("sudo systemctl mask NetworkManager-wait-online.service".split(), check=False)
     
-    # Tenta desabilitar o serviço NetworkManager-wait-online.service para evitar travamento de inicialização
-    subprocess.run("sudo systemctl disable NetworkManager-wait-online.service".split(), check=False)
     try:
         subprocess.run(["timedatectl", "set-timezone", "America/Sao_Paulo"], check=True)
         subprocess.run(["timedatectl", "set-ntp", "true"], check=True)
