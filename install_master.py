bea17c78dba1d658978f8ac4b47a87837cc088c8
Mauricio Azeredo <mauriciowebme@gmail.com>
2026-06-02 13:25:52 -0300

    fix: corrige Type do systemd de simple para forking no VNC server

--- install_master.py
+++ install_master.py
@@ -6088,11 +6088,12 @@ class Sistema(Docker, Executa_comandos):
             After=syslog.target network.target
             
             [Service]
-            Type=simple
+            Type=forking
             User={user}
             Group={user}
             Environment=HOME={home_dir}
             WorkingDirectory={home_dir}
+            PIDFile={home_dir}/.vnc/%H%i.pid
             ExecStartPre=/bin/sh -c '/usr/bin/vncserver -kill {display} > /dev/null 2>&1 || :'
             ExecStart=/usr/bin/vncserver {display} -geometry {resolution} -depth 24 -localhost no
             ExecStop=/usr/bin/vncserver -kill {display}
